8066e7554a405a767ddc60a3ecf4c734f172f1a7
Gerhard Sletten <gerhardsletten@gmail.com>
2018-02-06 01:31:02 +0100

    Only load tracking if in prod

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import Script from '../elements/Script'
 import config from '../config'
+const prod = process.env.NODE_ENV === 'production'
 
 export default class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
@@ -18,23 +19,25 @@ export default class MyDocument extends Document {
           {this.props.styleTags}
           <link rel='shortcut icon' href={config.favicon} />
           <meta name='viewport' content='width=device-width' />
-          <Script>
-            {
-              () => {
-                if ('serviceWorker' in navigator) {
-                  navigator.serviceWorker
-                    .register('/service-worker.js')
-                    .then(registration => {
-                      console.log('service worker registration successful')
-                    })
-                    .catch(err => {
-                      console.warn('service worker registration failed', err.message)
-                    })
+          {prod && (
+            <Script>
+              {
+                () => {
+                  if ('serviceWorker' in navigator) {
+                    navigator.serviceWorker
+                      .register('/service-worker.js')
+                      .then(registration => {
+                        console.log('service worker registration successful')
+                      })
+                      .catch(err => {
+                        console.warn('service worker registration failed', err.message)
+                      })
+                  }
                 }
               }
-            }
-          </Script>
-          {config.tagManager && (
+            </Script>
+          )}
+          {prod && config.tagManager && (
             <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
