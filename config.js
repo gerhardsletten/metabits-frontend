7ca0a8c5fb570bb9811707acf9b9d8cbf05eec57
Gerhard Sletten <gerhardsletten@gmail.com>
2018-02-13 00:53:16 +0100

    Syntax and version for persist-apollo

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 module.exports = {
   wpApi: process.env.DB_API || 'http://localhost:3310',
-  servicePath : process.env.SERVICE_PATH || '/tjenester',
-  portfolioPath : process.env.PORTFOLIO_PATH || '/referanser',
+  servicePath: process.env.SERVICE_PATH || '/tjenester',
+  portfolioPath: process.env.PORTFOLIO_PATH || '/referanser',
   techPath: process.env.TECH_PATH || '/teknologier',
   favicon: 'https://s3.eu-central-1.amazonaws.com/metabits-static/layout/favicon.ico',
   logo: 'https://s3.eu-central-1.amazonaws.com/metabits-static/layout/logo-metabits-plain.svg',
   publicUrl: process.env.PUBLIC_URL || 'http://localhost:3000',
-  tagManager: process.env.TAG_MANAGER || 'GTM-5MC8TVT'
+  tagManager: process.env.TAG_MANAGER || 'GTM-5MC8TVT',
+  contentVersion: process.env.CONTENT_VERSION || 'v2'
 }
